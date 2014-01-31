1cf3897430df8af007fa1cd1727d1937af16a1d0
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-31 15:05:20 -0800

    Added basic routes and index page

---
+++ flash_card/app/controllers/index.rb
@@ -0,0 +1,28 @@
+get '/' do
+
+  erb :index
+end
+
+get '/deck/:id/quiz' do
+
+end
+
+get '/deck/:id/quiz/correct' do
+
+end
+
+get '/deck/:id/quiz/incorrect' do
+
+end
+
+post '/deck/:id/quiz' do
+
+end
+
+get '/score' do
+
+end
+
+post '/quit' do
+
+end
