9099731419b779c910b777b2fec6198b0a034141
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-31 15:20:01 -0800

    Added answer validation and views

--- flash_card/app/controllers/index.rb
+++ flash_card/app/controllers/index.rb
@@ -11,15 +11,21 @@ erb :quiz
 end
 
 get '/deck/:id/quiz/correct' do
-
+ "Correct"
 end
 
 get '/deck/:id/quiz/incorrect' do
-
+"Incorrect"
 end
 
 post '/deck/:id/quiz' do
-
+  @deck = Deck.find(params[:id])
+  @top_card = @deck.cards.first
+  if params[:guess].downcase == @top_card.answer.downcase
+    redirect "/deck/#{params[:id]}/quiz/correct"
+  else
+    redirect "/deck/#{params[:id]}/quiz/incorrect"
+  end
 end
 
 get '/score' do
