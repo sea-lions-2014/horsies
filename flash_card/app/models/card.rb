f49bae1ac660c7f58dbca3cb1ad565eedc2380de
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-31 14:47:55 -0800

    changed migration files and associations

--- flash_card/app/models/card.rb
+++ flash_card/app/models/card.rb
@@ -1,4 +1,5 @@
 class Card < ActiveRecord::Base
+  has_many :cardecks
 	has_many :decks, through: :cardecks
   # Remember to create a migration!
 end
--- flash_card/db/migrate/20140131120329_create_cards.rb
+++ flash_card/db/migrate/20140131120329_create_cards.rb
@@ -1,6 +1,6 @@
 class CreateCards < ActiveRecord::Migration
   def change
-  	create_table :CreateCards do |t|
+  	create_table :cards do |t|
   		t.string :question, :answer
   	end
   end
