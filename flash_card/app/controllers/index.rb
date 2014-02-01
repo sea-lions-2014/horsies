5f44e0e6ec96c5e5a664e8425bc4d9b626265a19
Eli Shkurkin <eshkurkin@gmail.com>
2014-01-31 16:06:53 -0800

    Modified routes so that cards are able to be created from deck_view

--- flash_card/app/controllers/index.rb
+++ flash_card/app/controllers/index.rb
@@ -1,12 +1,46 @@
-get '/create_card' do
+get '/' do
+  @decks = Deck.all
+  erb :index
+end
 
+get '/deck/:deck_id' do
+  @deck = Deck.find(params[:deck_id])
+  @cards = @deck.cards
+  erb :deck_view
+end
+
+get '/deck/:deck_id/card/new' do
+  @deck_id = params[:deck_id]
   erb :create_card
 end
 
-post '/created' do
+get '/deck/new' do
+  erb :create_deck
+end
+
+post '/deck/:deck_id/card/create' do
   @question = params[:question]
   @answer = params[:answer]
   @category = params[:category]
-  # @new_card = Card.new(@question, @answer, @category)
-  erb :created
+  @deck_id = params[:deck_id]
+  @type = "card"
+  @new_card = Card.create(question: @question, answer: @answer, category: @category)
+  @new_card.decks << Deck.find(params[:deck_id])
+  redirect '/card_got_created'
 end
+
+post '/deck/create' do
+  @name = params[:deck_name]
+  @type = "deck"
+  @new_deck = Deck.create(name: @name)
+  redirect '/deck_got_created'
+end
+
+get '/card_got_created' do
+  erb :created_card
+end
+
+get '/deck_got_created' do
+  erb :created_deck
+end
+
