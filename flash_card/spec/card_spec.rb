4d8c0c34d68865d8dec2cd760a734c2b38d0f18a
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-31 18:38:35 -0800

    Add spec test for card deck association

--- flash_card/spec/card_spec.rb
+++ flash_card/spec/card_spec.rb
@@ -2,17 +2,35 @@ require_relative 'spec_helper'
 
 describe Card do
   describe "card creation" do
-    before do
-      @card = Card.new(question: 'what is andys age', answer: '19')
-    end
+    let(:deck) { Deck.create(name: 'Andy') }
+    let(:card) { Card.new(question: 'what is andys age', answer: '19') }
+
 
     it "should have an answer" do
-      expect(@card.answer).to eq ('19')
+      expect(card.answer).to eq ('19')
     end
 
     it "should have a question" do
-      expect(@card.question).to eq ('what is andys age')
+      expect(card.question).to eq ('what is andys age')
     end
+
+    it "should belong to one or more decks" do
+      expect(card.decks.first).to eq (@deck)
+    end
+  end
+
+  describe "card deck association" do
+    let(:deck) { Deck.create(name: 'Andy') }
+    let(:card) { Card.create(question: 'what is andys age', answer: '19') }
+
+    before :each do
+      card.decks << deck
+    end
+
+    it "should have access to deck" do
+      expect(card.decks.first.name).to eq('Andy')
+    end
+
   end
 
 end
