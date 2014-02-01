f0608c5aec2955d1e1bc04b9c7e7baa72b8b2097
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-31 16:01:45 -0800

    Add basic seeder to populate db with deck

--- flash_card/app/controllers/index.rb
+++ flash_card/app/controllers/index.rb
@@ -3,12 +3,25 @@ get '/' do
   erb :index
 end
 
-get '/deck/:id/quiz' do
-@deck = Deck.find(params[:id])
+get '/deck/andy/quiz' do
+  puts "hi andy"
+@deck = Deck.where(name: 'Andy').first
 @top_card = @deck.cards.first
 
 erb :quiz
 end
+
+# get '/deck/:id/quiz' do
+
+# @deck = Deck.find(params[:id])
+#   puts "hi #{@deck.name} asdf"
+# @top_card = @deck.cards.first
+#   puts "hi first card #{@top_card.question}"
+
+# erb :quiz
+# end
+
+
 
 get '/deck/:id/quiz/correct' do
  "Correct"
@@ -26,6 +39,7 @@ post '/deck/:id/quiz' do
   else
     redirect "/deck/#{params[:id]}/quiz/incorrect"
   end
+  @deck.card.rotate
 end
 
 get '/score' do
