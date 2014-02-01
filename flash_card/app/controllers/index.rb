2fd35ae7867fc839c4dbff7d2534c11e554c1fc5
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-31 16:49:51 -0800

    Add quiz and random q functionality

--- flash_card/app/controllers/index.rb
+++ flash_card/app/controllers/index.rb
@@ -1,49 +1,46 @@
+enable :sessions
+
 get '/' do
 
   erb :index
 end
 
-get '/deck/andy/quiz' do
-  puts "hi andy"
-@deck = Deck.where(name: 'Andy').first
-@top_card = @deck.cards.first
+get '/deck/:id/quiz' do
+  @deck = Deck.find(params[:id])
+  puts "hi #{@deck.name} asdf"
+  @top_card = @deck.cards.shuffle.first
+  session[:card_id] = @top_card.id
+  puts "hi first card #{session[:card_id]}"
 
-erb :quiz
+  erb :quiz
 end
 
-# get '/deck/:id/quiz' do
-
-# @deck = Deck.find(params[:id])
-#   puts "hi #{@deck.name} asdf"
-# @top_card = @deck.cards.first
-#   puts "hi first card #{@top_card.question}"
-
-# erb :quiz
-# end
-
-
-
 get '/deck/:id/quiz/correct' do
- "Correct"
+
+ erb :correct
 end
 
 get '/deck/:id/quiz/incorrect' do
-"Incorrect"
+
+erb :incorrect
 end
 
 post '/deck/:id/quiz' do
+  puts "hi card on post #{session[:card_id]}"
   @deck = Deck.find(params[:id])
-  @top_card = @deck.cards.first
+  @top_card = Card.find(session[:card_id])
+  puts "hi card on post #{@top_card.answer}"
+  puts "hi card on post #{params[:guess]}"
   if params[:guess].downcase == @top_card.answer.downcase
     redirect "/deck/#{params[:id]}/quiz/correct"
   else
     redirect "/deck/#{params[:id]}/quiz/incorrect"
   end
-  @deck.card.rotate
 end
 
-get '/score' do
-
+get '/deck/:id/quiz/score' do
+  @id = params[:id]
+  erb :score
 end
 
 post '/quit' do
