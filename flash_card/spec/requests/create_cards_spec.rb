01fd63e7d8e879f620bf2fd5d584a7bd0e55e000
K Y <kevin@kevinyeh.com>
2014-02-02 17:54:46 -0800

    Add test to check for return after card creation

--- flash_card/spec/requests/create_cards_spec.rb
+++ flash_card/spec/requests/create_cards_spec.rb
@@ -3,7 +3,7 @@ require_relative '../acceptance_helper'
 describe 'Deck management' do
 	let(:user) { FactoryGirl.create(:user) }
 
-	before do
+	before :each do
 		valid_signin user
 	end
 
@@ -20,6 +20,44 @@ describe 'Deck management' do
 		it 'should have a link to create a new deck' do
 			expect(page).to have_content("new deck")
 		end
-	end
 
+		describe 'create a new deck' do
+			let(:new_deck_title) { "new deck" }
+
+			before do
+				click_link "Create a new deck"
+				fill_in 'deck_name', with: new_deck_title
+				click_button "Create!"
+			end
+
+			it 'should show success message' do
+				expect(page).to have_content("You have made a deck!")
+			end
+
+			it 'should have a link back to the deck list' do
+				expect(page).to have_content("Back")
+			end
+
+			describe 'return to new deck list' do
+				before do
+					click_link "Back"
+				end
+
+				it 'should return you to the deck list' do
+					expect(page).to have_content("Create a new deck")
+				end
+
+				it 'should show the new deck created' do
+					expect(page).to have_content(new_deck_title)
+				end
+			end
+		end
+
+		describe 'add cards to a deck' do
+			before do
+			end
+
+			
+		end
+	end
 end
