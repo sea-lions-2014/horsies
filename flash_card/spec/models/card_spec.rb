c46299981a11ba718fa8d3e1e8d65127b3e3789f
Steven G. Harms <steven@devbootcamp.com>
2014-02-04 02:24:09 -0800

    Add spec for add_to_deck!

--- flash_card/spec/models/card_spec.rb
+++ flash_card/spec/models/card_spec.rb
@@ -7,9 +7,11 @@ describe Card do
 
   subject { @card } 
 
+
   it { should respond_to(:question) }
   it { should respond_to(:answer) }
   it { should respond_to(:decks) }
+  it { should respond_to(:add_to_deck!) }
 
   describe "card deck association" do
     let(:deck) { FactoryGirl.create(:deck) }
@@ -21,6 +23,15 @@ describe Card do
     its(:card) { should eq @card }
     its(:deck) { should eq deck }
   end
+
+  it "should accumulate a deck" do
+    deck = FactoryGirl.create(:deck)
+
+    @card.add_to_deck!(deck)
+    @card.decks.should_not be_empty
+    @card.decks.should include(deck)
+  end
+
 end
 
 describe Deck do
