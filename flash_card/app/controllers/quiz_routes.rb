d12640ae2b8aebdb89abccca5147bd8bbbf1ee94
Steven G. Harms <steven@devbootcamp.com>
2014-02-04 03:35:40 -0800

    Refactor:  Push logic on to Deck

--- flash_card/app/controllers/quiz_routes.rb
+++ flash_card/app/controllers/quiz_routes.rb
@@ -16,16 +16,9 @@ get '/deck/:id/quiz/:num' do
 end
 
 post '/deck/:id/quiz/:num' do
-  @num += 1
-  @score = Score.create(user_id: @user.id, deck_id: params[:id].to_i, value: 0) if @num == 1 # create a new score on the first question
-  session[:score] = 0 if params[:num].to_i == 0 #set session score value to zero if on the first question
-  @score = Score.where(user_id: @user.id, deck_id: params[:id].to_i).last # find the score object if you're not on the first question
-  @top_card = Card.find(session[:card_id])
-  @correct = params[:guess].downcase == @top_card.answer.downcase ? true : false
-  session[:score] += 1 if @correct # add one to score if correct
-  @score.update_attributes(value: session[:score])
+  @deck.track_guess(params, @user, session)
 
-  if params[:num].to_i == @deck.cards.length - 1 # if last question
+  if @deck.last_question?(params[:num].to_i )
     erb :user_score # show score
   else
     erb :quiz_feedback # or show feedback
--- flash_card/app/models/deck.rb
+++ flash_card/app/models/deck.rb
@@ -3,5 +3,22 @@ class Deck < ActiveRecord::Base
 	has_many :cards, through: :cardecks
 	belongs_to :user
   has_many :scores
-  # Remember to create a migration!
+
+  def user_has_no_score_for_me?(user)
+    Score.where(user_id: user.id, deck_id: self.id).empty?
+  end
+
+  def track_guess(params, user, session)
+    @score = Score.create(user_id: user.id, deck_id: params[:id].to_i, value: 0) if user_has_no_score_for_me?(user)
+    session[:score] = 0 if params[:num].to_i == 0 #set session score value to zero if on the first question
+    @score = Score.where(user_id: user.id, deck_id: params[:id].to_i).last # find the score object if you're not on the first question
+    @top_card = Card.find(session[:card_id])
+    @correct = params[:guess].downcase == @top_card.answer.downcase ? true : false
+    session[:score] += 1 if @correct # add one to score if correct
+    @score.update_attributes(value: session[:score])
+  end
+
+  def last_question?(card_number)
+    card_number == self.cards.length - 1
+  end
 end
