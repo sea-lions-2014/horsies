3491efa3b4ecb825260a37e825fec35818097663
Steven G. Harms <steven@devbootcamp.com>
2014-02-04 03:42:01 -0800

    Refactor: extract get_score_for_user, initalize_session_score

--- flash_card/app/models/deck.rb
+++ flash_card/app/models/deck.rb
@@ -8,10 +8,22 @@ class Deck < ActiveRecord::Base
     Score.where(user_id: user.id, deck_id: self.id).empty?
   end
 
-  def track_guess(params, user, session)
-    @score = Score.create(user_id: user.id, deck_id: params[:id].to_i, value: 0) if user_has_no_score_for_me?(user)
+  def get_score_for_user(user)
+    if user_has_no_score_for_me?(user)
+      Score.create(user_id: user.id, deck_id: self.id, value: 0)
+    else
+      Score.where(user_id: user.id, deck_id: self.id).last
+    end
+  end
+
+  def initalize_session_score!(params, session)
     session[:score] = 0 if params[:num].to_i == 0 #set session score value to zero if on the first question
-    @score = Score.where(user_id: user.id, deck_id: params[:id].to_i).last # find the score object if you're not on the first question
+  end
+
+
+  def track_guess(params, user, session)
+    @score = get_score_for_user(user)
+    initalize_session_score!(params, session)
     @top_card = Card.find(session[:card_id])
     @correct = params[:guess].downcase == @top_card.answer.downcase ? true : false
     session[:score] += 1 if @correct # add one to score if correct
